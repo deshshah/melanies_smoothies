315ff848c61b189167c60774cf7c7cbc2e514b6e
deshshah <deshshah@nhs.net>
2025-11-11 21:01:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #fruit_list = fruit_df.to_pandas()['FRUIT_NAME'].tolist()
-#fruit_list = pd_df['FRUIT_NAME'].tolist()
+fruit_list = pd_df['FRUIT_NAME'].tolist()
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
-ingredients_selected = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
