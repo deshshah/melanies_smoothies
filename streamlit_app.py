cf519ad32202e071eeee866b9427ced67bd518b1
deshshah <deshshah@nhs.net>
2025-11-11 16:14:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,20 +16,22 @@ fruit_list = fruit_df.to_pandas()['FRUIT_NAME'].tolist()
 
 ingredients_selected = st.multiselect("Choose up to 5 ingredients:", fruit_list, max_selections=5)
 
-if ingradients_lists: 
-   ingredients_string =''
+if ingredients_selected:
+    ingredients_string = ''
 
-       for fruit_chosen in ingradients_lists:
-          ingredients_string += fruit_chosen + ' '
-          st.subheader(fruit_chosen + 'Nutrition Information')
-          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+ fruit+chosen)
-          #st.text(smoothiefroot_response.jeson())
-          sf_df =st.dataframe(data=smoothiefroot_response.jeson(), use_container_width=True)
-          my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-          values ('""" +ingredients_string+ """','""" +name_on_order+ """')"""
-           time_to_insert = st.button('Submit Order')
-       if time_to_insert:
-           session.sql(my_insert_stmt).collect()
-           st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+    for fruit_chosen in ingredients_selected:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
 
     
