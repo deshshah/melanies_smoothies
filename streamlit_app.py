a746dd4dce4f8e7606764ea47750260fad2638c5
Desh Shah <deshshah@nhs.net>
2026-04-14 16:23:30 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
    
 
@@ -46,7 +48,6 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 
